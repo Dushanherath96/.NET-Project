0479ed595f019c4d9ab6aa8dd731f3f59dec0979
dush <dushan1855@gmail.com>
2025-11-11 19:23:17 +0530

    DB set

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Configure SQLite database connection
 //connection string
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStor");
+//register DbContext with DI container
 builder.Services.AddSqlite<GameStore.Api.Data.GameStoreContext>(connString);
 
 var app = builder.Build();
